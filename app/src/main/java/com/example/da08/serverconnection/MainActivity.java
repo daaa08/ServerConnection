dae7e50a3a0b2a957673af64f1cbb4ce3d4782ef
박다정 <daaa08@naver.com>
2017-06-29 17:12:58 +0900

    annotation

--- app/src/main/java/com/example/da08/serverconnection/MainActivity.java
+++ app/src/main/java/com/example/da08/serverconnection/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.da08.serverconnection;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 
 import org.androidannotations.annotations.EActivity;
 
 @EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
+
+    public void goWrite(){
+        Intent intent = new Intent(this, WriteActivity.class);
+        startActivity(intent);
+    }
 
 }
--- app/src/main/java/com/example/da08/serverconnection/WriteActivity.java
+++ app/src/main/java/com/example/da08/serverconnection/WriteActivity.java
@@ -6,31 +6,27 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 public class WriteActivity extends AppCompatActivity {
 
-    @BindView(R.id.editTitle)
+
     EditText editTitle;
-    @BindView(R.id.editAuthor)
+
     EditText editAuthor;
-    @BindView(R.id.editContent)
+
     EditText editContent;
-    @BindView(R.id.btnWrite)
+
     Button btnWrite;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write);
-        ButterKnife.bind(this);
+
 
 
     }
 
-    @OnClick(R.id.btnWrite)
     public void onClick(){
         Toast.makeText(this,"yoyo",Toast.LENGTH_SHORT).show();
     }
