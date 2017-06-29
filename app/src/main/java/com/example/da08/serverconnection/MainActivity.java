331e7452e3d0323399c96326b7c5ddd363eb6e1a
박다정 <daaa08@naver.com>
2017-06-29 17:06:42 +0900

    annotation

--- app/src/main/java/com/example/da08/serverconnection/MainActivity.java
+++ app/src/main/java/com/example/da08/serverconnection/MainActivity.java
@@ -1,31 +1,10 @@
 package com.example.da08.serverconnection;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
+import org.androidannotations.annotations.EActivity;
 
+@EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.recyclerView)
-    RecyclerView recyclerView;
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
-
-    }
-
-    @OnClick(R.id.btnPost)  // 클릭리스너를 온클릭 한줄로 바꿔 줌
-    public void goWrite(){
-        Intent intent = new Intent(this, WriteActivity.class);
-        startActivity(intent);
-    }
 }
