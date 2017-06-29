d5d25da5f5b55098b46bdd8f013af7700abedb86
박다정 <daaa08@naver.com>
2017-06-29 17:05:00 +0900

    Initial commit

---
+++ app/src/main/java/com/example/da08/serverconnection/domain/Bbs.java
@@ -0,0 +1,13 @@
+package com.example.da08.serverconnection.domain;
+
+/**
+ * Created by Da08 on 2017. 6. 26..
+ */
+
+public class Bbs {
+    int id;
+    String title;
+    String author;
+    String content;
+    String date;
+}
---
+++ app/src/main/java/com/example/da08/serverconnection/domain/Data.java
@@ -0,0 +1,12 @@
+package com.example.da08.serverconnection.domain;
+
+import java.util.List;
+
+/**
+ * Created by Da08 on 2017. 6. 26..
+ */
+
+public class Data {
+
+    List<Bbs> bbsList;
+}
