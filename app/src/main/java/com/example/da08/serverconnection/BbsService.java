0356ad37c9aa6881a32cad2bc0cfca4e530d9cf0
박다정 <daaa08@naver.com>
2017-06-30 14:15:29 +0900

    retrofit

---
+++ app/src/main/java/com/example/da08/serverconnection/BbsService.java
@@ -0,0 +1,17 @@
+package com.example.da08.serverconnection;
+
+import com.example.da08.serverconnection.domain.Bbs;
+import com.example.da08.serverconnection.domain.Result;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+/**
+ * Created by Da08 on 2017. 6. 30..
+ */
+
+public interface BbsService {
+    @POST("Bbs")
+    Call<Result> createBbs(@Body Bbs bbs);
+}
---
+++ app/src/main/java/com/example/da08/serverconnection/domain/Result.java
@@ -0,0 +1,9 @@
+package com.example.da08.serverconnection.domain;
+
+/**
+ * Created by Da08 on 2017. 6. 30..
+ */
+
+public class Result {
+    public String result;
+}
